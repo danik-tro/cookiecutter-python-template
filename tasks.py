7fd993360113ba4c0555eedbcfd053c60e7303a7
Daniil Trotsenko <daniil.trotsenko.dev@gmail.com>
2022-07-21 23:20:28 +0300

    Add poetry dependencies

--- tasks.py
+++
@@ -1,9 +0,0 @@
-from invoke import task
-import os 
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-@task
-def build(ctx):
-    """Build the cookiecutter."""
-    ctx.run(f"python cookiecutter_spec.py {HERE} --no-input")
